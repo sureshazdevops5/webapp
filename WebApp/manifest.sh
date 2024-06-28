4c62cefc331e4b3a4236ecf18318d976f8ee6b97
sureshazdevops5 <sureshazdevops5@gmail.com>
2024-06-28 14:41:26 +0000

    Updated manifest.sh

--- WebApp/manifest.sh
+++ WebApp/manifest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x REPO_URL = "https://uutxfci222nbrlmyuzjm472kyob4jzncibkzib6nbxgf423ytaxq@dev.azure.com/sureshazdevops5/webapp/_git/webapp"
+mkdir /tmp/temp_repo
 git clone "$REPO_URL" /tmp/temp_repo
 cd /tmp/temp_repo
 sed -i "s|image:.*|image:testsureshacr.azurecr.io/$2:$3|g" WebApp/$1-service.yaml
