990bef70d7a7b96dae767d3d84efdda5b11b0804
sureshazdevops5 <sureshazdevops5@gmail.com>
2024-06-28 12:54:01 +0000

    Updated updatemanifest.sh

--- WebApp/updatemanifest.sh
+++ WebApp/updatemanifest.sh
@@ -1,11 +1,34 @@
-#!.bin.bash
+#!/bin/bash
+
+# Convert line endings to Unix format
+dos2unix /home/vsts/work/1/s/WebApp/updatemanifest.sh
 
 set -x
 
-REPO_URL= "https://uutxfci222nbrlmyuzjm472kyob4jzncibkzib6nbxgf423ytaxq@dev.azure.com/sureshazdevops5/webapp/_git/webapp"
+# Set Git user identity
+git config --global user.email "sureshazdevops5@gmail.com"
+git config --global user.name "Suresh Nunna"
 
-git clone "$REPO_URL" /temp/temp_repo
+# Set the repository URL (ensure PAT is correctly handled)
+REPO_URL="https://uutxfci222nbrlmyuzjm472kyob4jzncibkzib6nbxgf423ytaxq@dev.azure.com/sureshazdevops5/webapp/_git/webapp"
 
-#cd /temp/temp_repo
+# Clone the git repository into the /tmp directory
+git clone "$REPO_URL" /tmp/temp_repo
 
-#sed -i "s|image:.*|image: testsureshacr.azurecr.io/$2:$3|g" /WebApp/$1-service.yaml
+# Navigate into the cloned repository directory
+cd /tmp/temp_repo
+
+# Make changes to the Kubernetes manifest file(s)
+sed -i "s|image:.*|image: testsureshacr.azurecr.io/$2:$3|g" $1-service.yaml
+
+# Add the modified files
+git add .
+
+# Commit the changes
+git commit -m "Update Kubernetes manifest"
+
+# Push the changes back to the repository
+git push
+
+# Cleanup: remove the temporary directory
+rm -rf /tmp/temp_repo
