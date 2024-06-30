4036a961e47b7e71aa94f9421892279747e39776
sureshazdevops5 <sureshazdevops5@gmail.com>
2024-06-30 10:01:52 +0000

    Updated manifest.sh

--- WebApp/manifest.sh
+++ WebApp/manifest.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Set the repository URL
-REPO_URL="https://<ACCESS-TOKEN>@dev.azure.com/<AZURE-DEVOPS-ORG-NAME>/voting-app/_git/voting-app"
+REPO_URL="https://cccq6o4fmhqvd6najjscvh7d4ljjbb2aen6fnbvip4cz34jmk2ea@dev.azure.com/sureshazdevops5/webapp/_git/webapp"
 
 # Clone the git repository into the /tmp directory
 git clone "$REPO_URL" /tmp/temp_repo
@@ -13,7 +13,7 @@ cd /tmp/temp_repo
 
 # Make changes to the Kubernetes manifest file(s)
 # For example, let's say you want to change the image tag in a deployment.yaml file
-sed -i "s|image:.*|image: <ACR-REGISTRY-NAME>/$2:$3|g" k8s-specifications/$1-deployment.yaml
+sed -i "s|image:.*|image: testsureshacr.azurecr.io/$2:$3|g" $1-service.yaml
 
 # Add the modified files
 git add .
