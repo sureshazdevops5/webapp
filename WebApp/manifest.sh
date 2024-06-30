075b308e5a490845316bac03e405f1e29de9f060
sureshazdevops5 <sureshazdevops5@gmail.com>
2024-06-30 08:16:17 +0000

    Updated manifest.sh

--- WebApp/manifest.sh
+++ WebApp/manifest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -X
+git config --global user.email "sureshazdevops5@gmail.com"
+git config --global user.name "azure devops"
 #REPO_URL="https://cccq6o4fmhqvd6najjscvh7d4ljjbb2aen6fnbvip4cz34jmk2ea@dev.azure.com/sureshazdevops5/webapp/_git/webapp"
 git clone https://cccq6o4fmhqvd6najjscvh7d41jjbb2aen6fnbvip4cz34jmk2ae@dev.azure.com/sureshazdevops5/webapp/_git/webapp /tmp/temp_repo
 cd /tmp/temp_repo
